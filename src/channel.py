342c3ee0cf2a527c6377d9ba50b4301a5d194f49
МОЙ <kerik006@mail.ru>
2023-06-05 01:19:44 +0300

    coomit

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,16 @@
 
-
 class Channel:
-    """Класс для ютуб-канала"""
+    def __init__(self, channel_id):
+        self.channel_id = channel_id
+        self.api_key = 'nYWC2jEhe6ymWQkZPzVzBMkaxWnosnka'
 
-    def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+    def get_channel_info(self):
+        import requests
+        url = f'https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={self.channel_id}&key={self.api_key}'
+        response = requests.get(url)
+        data = response.json()
+        return data
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+    def print_info(self):
+        channel_info = self.get_channel_info()
+        print (channel_info)
