c1a07d32702161c08ae881cff49bd15dc85f384c
PC <kerik006@mail.ru>
2023-07-30 21:01:15 +0400

    homework 2

--- src/channel.py
+++ src/channel.py
@@ -8,13 +8,21 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
+    @classmethod
     def get_service(cls):
         return build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
 
-    def __printj(self, dict_to_print: dict) -> None:
+    @staticmethod
+    def __printj(dict_to_print: dict) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
         print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         self.__printj(self.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute())
+
+    @staticmethod
+    def to_json(name):
+        with open(name, 'w') as outfile:
+            json.dump(name, outfile)
+
