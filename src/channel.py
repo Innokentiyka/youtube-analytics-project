ffa51139d3c3ab724fedd7a7b4c0f1e3bda332b8
PC <kerik006@mail.ru>
2023-07-31 21:37:02 +0400

    homework two

--- src/channel.py
+++ src/channel.py
@@ -2,27 +2,61 @@ import json
 import os
 from googleapiclient.discovery import build
 
-
 class Channel:
-    def __init__(self, channel_id: str) -> None:
+    """Класс для ютуб-канала"""
+
+    youtube = None
+
+    def __init__(self, channel_id):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.API_KEY = os.getenv('YT_API_KEY')
+        self.youtube = build('youtube', 'v3', developerKey=self.API_KEY)
+        self.__channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
-    @classmethod
-    def get_service(cls):
-        return build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.url = f'https://www.youtube.com/channel/{self.channel_id}'
+        self.subscriberCount = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.viewCount = self.channel["items"][0]["statistics"]["viewCount"]
 
-    @staticmethod
-    def __printj(dict_to_print: dict) -> None:
-        """Выводит словарь в json-подобном удобном формате с отступами"""
-        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+    def __str__(self):
+        return f'{self.title} ({self.url}'
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        self.__printj(self.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute())
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.channel = json.dumps(self.channel, indent=2, ensure_ascii=False)
+        print(f'{self.channel}')
 
-    @staticmethod
-    def to_json(name):
-        with open(name, 'w') as outfile:
-            json.dump(name, outfile)
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
+    @channel_id.setter
+    def channel_id(self, name):
+        if isinstance(name, str):
+            self.__name = name
+        else:
+            print("False name or invalid")
+
+    @classmethod
+    def get_service(cls):
+        id_channel = input("Введите id канала: ")
+        youtube = build('youtube', 'v3', developerKey='AIzaSyDAKyCWtI_f-A9-ob8OYtx_YqdOVEC-gRU')
+        channel = youtube.channels().list(id=id_channel, part='snippet,statistics').execute()
+        youtube_channel = Channel(id_channel)
+        return f'Канал: {channel}' \
+               f'Создан объект класса "Channel" {youtube_channel}'
+
+    @classmethod
+    def to_class(cls, obj):
+        if not isinstance(obj, Channel):
+            raise TypeError('Операнд справа должен быть экземпляром класса '
+                            'Channel!')
+        return obj.subscriberCount
+
+    def to_json(self, file_name=None):
+        with open(file_name, 'w') as file:
+            json.dump(self.channel, file)
